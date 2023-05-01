5dd3266930079735d88ae03613486d85be84312e
fl-korbes <felipekorbes@live.com>
2023-04-30 21:27:26 -0300

    :white_check_mark: atualizada tb do next_state

--- utils/cu/next_state.v
+++ utils/cu/next_state.v
@@ -6,28 +6,28 @@ module next_state (
     wire wire0, wire1, wire3, wire4, wire5, wire6, wire7, wire8, 
          wire9, wire10, wire11, wire12, wire13, wire14, wire15;    
    
-    and (wire0, ~op[5], ~op[4], ~op[3], ~op[2], op[1], ~op[0], ~state[3], ~state[2], ~state[2], state);
-    and (wire1, ~op[5], ~op[4], ~op[3], op[2], ~op[1], ~op[0], ~state[3], ~state[2], ~state[2], state);
+    and (wire0, ~op[5], ~op[4], ~op[3], ~op[2], op[1], ~op[0], ~state[3], ~state[2], ~state[1], state[0]);
+    and (wire1, ~op[5], ~op[4], ~op[3], op[2], ~op[1], ~op[0], ~state[3], ~state[2], ~state[1], state[0]);
     or (ns[3], wire0, wire1);
 
-    and (wire2, ~op[5], ~op[4], ~op[3], ~op[2], ~op[1], ~op[0], ~state[3], ~state[2], ~state[2], state);
-    and (wire3, op[5], ~op[4], op[3], ~op[2], op[1], op[0], ~state[3], ~state[2], state[2], ~state);
-    and (wire4, ~state[3], ~state[2], state[2], state);
-    and (wire5, ~state[3], state[2], state[2], ~state); 
+    and (wire2, ~op[5], ~op[4], ~op[3], ~op[2], ~op[1], ~op[0], ~state[3], ~state[2], ~state[1], state[0]);
+    and (wire3, op[5], ~op[4], op[3], ~op[2], op[1], op[0], ~state[3], ~state[2], state[1], ~state[0]);
+    and (wire4, ~state[3], ~state[2], state[1], state[0]);
+    and (wire5, ~state[3], state[2], state[1], ~state[0]); 
     or (ns[2], wire2, wire3, wire4, wire5);
 
-    and (wire6, ~op[5], ~op[4], ~op[3], ~op[2], ~op[1], ~op[0], ~state[3], ~state[2], ~state[2], state);
-    and (wire7, op[5], ~op[4], ~op[3], ~op[2], op[1], op[0], ~state[3], ~state[2], ~state[2], state);
-    and (wire8, op[5], ~op[4], op[3], ~op[2], op[1], op[0], ~state[3], ~state[2], ~state[2], state);
-    and (wire9, op[5], ~op[4], ~op[3], ~op[2], op[1], op[0], ~state[3], ~state[2], state[2], ~state);
-    and (wire10, ~state[3], state[2], state[2], ~state); 
+    and (wire6, ~op[5], ~op[4], ~op[3], ~op[2], ~op[1], ~op[0], ~state[3], ~state[2], ~state[1], state[0]);
+    and (wire7, op[5], ~op[4], ~op[3], ~op[2], op[1], op[0], ~state[3], ~state[2], ~state[1], state[0]);
+    and (wire8, op[5], ~op[4], op[3], ~op[2], op[1], op[0], ~state[3], ~state[2], ~state[1], state[0]);
+    and (wire9, op[5], ~op[4], ~op[3], ~op[2], op[1], op[0], ~state[3], ~state[2], state[1], ~state[0]);
+    and (wire10, ~state[3], state[2], state[1], ~state[0]); 
     or (ns[1], wire6, wire7, wire8, wire9, wire10);
 
-    and (wire11, ~state[3], ~state[2], ~state[2], ~state);
-    and (wire12, op[5], ~op[4], ~op[3], ~op[2], op[1], op[0], ~state[3], ~state[2], state[2], ~state);
-    and (wire13, op[5], ~op[4], op[3], ~op[2], op[1], op[0], ~state[3], ~state[2], state[2], ~state);
-    and (wire14, ~state[3], state[2], state[2], ~state);
-    and (wire15, ~op[5], ~op[4], ~op[3], ~op[2], op[1], ~op[0], ~state[3], ~state[2], ~state[2], state); 
+    and (wire11, ~state[3], ~state[2], ~state[1], ~state[0]);
+    and (wire12, op[5], ~op[4], ~op[3], ~op[2], op[1], op[0], ~state[3], ~state[2], state[1], ~state[0]);
+    and (wire13, op[5], ~op[4], op[3], ~op[2], op[1], op[0], ~state[3], ~state[2], state[1], ~state[0]);
+    and (wire14, ~state[3], state[2], state[1], ~state[0]);
+    and (wire15, ~op[5], ~op[4], ~op[3], ~op[2], op[1], ~op[0], ~state[3], ~state[2], ~state[1], state[0]); 
     or (ns[0], wire11, wire12, wire13, wire14, wire15);
     
 endmodule
--- utils/cu/next_state_tb.v
+++ utils/cu/next_state_tb.v
@@ -1,15 +1,15 @@
 module next_state_tb ();
     reg [5:0] op;
-    reg s3, s2, s1, s0;
-    wire ns3, ns2, ns1, ns0;
+    reg [3:0] state;
+    wire [3:0] ns;
     
-    integer errors;
+    integer errors = 0;
 
     task Check1;
         input [7:0] expect;
         
         if (expect[7:4] !== expect[3:0]) begin
-            $display("Got %b, expected %b (State error)", expect[7:4], expect[3:0]);
+            $display("Got %b, expected %b (State or ns error)", expect[7:4], expect[3:0]);
             errors = errors + 1;
         end
     endtask
@@ -23,94 +23,94 @@ module next_state_tb ();
         end
     endtask
 
-    next_state UUT (.op(op), .s3(s3), .s2(s2), .s1(s1), .s0(s0), 
-                .ns3(ns3), .ns2(ns2), .ns1(ns1), .ns0(ns0));
+    next_state UUT (.op(op), .state(state), .ns(ns));
 
     initial begin
-        s3 = 0; s2 = 0; s1 = 0; s0 = 0;
+        state[3:0] = 4'b00_00;
         #5
         $display("Test #0");
-        Check1({s3, s2, s1, s0, 4'b00_00});
-        Check1({ns3, ns2, ns1, ns0, 4'b00_01});
+        Check1({state[3:0], 4'b00_00});
+        Check1({ns[3:0], 4'b00_01});
         #5
-
-        s3 = 0; s2 = 0; s1 = 0; s0 = 1;
-        op[5] = 0; op[4] = 0; op[3] = 0; op[2] = 0; op[1] = 1; op[0] = 0;
+        
+        state[3:0] = 4'b00_01;
+        op[5:0] = 6'b000_010;
         #5
         $display("Test #1");
-        Check1({s3, s2, s1, s0, 4'b00_01});
-        Check2({op[5], op[4], op[3], op[2], op[1], op[0], 6'b000_010});
-        Check1({ns3, ns2, ns1, ns0, 4'b10_01});
+        Check1({state[3:0], 4'b00_01});
+        Check2({op[5:0], 6'b000_010});
+        Check1({ns[3:0], 4'b10_01});
         #5
 
-        s3 = 0; s2 = 0; s1 = 0; s0 = 1;
-        op[5] = 0; op[4] = 0; op[3] = 0; op[2] = 1; op[1] = 0; op[0] = 0;
+        state[3:0] = 4'b00_01;
+        op[5:0] = 6'b000_100;
         #5
         $display("Test #2");
-        Check1({s3, s2, s1, s0, 4'b00_01});
-        Check2({op[5], op[4], op[3], op[2], op[1], op[0], 6'b000_100});
-        Check1({ns3, ns2, ns1, ns0, 4'b10_00});
+        Check1({state[3:0], 4'b00_01});
+        Check2({op[5:0], 6'b000_100});
+        Check1({ns[3:0], 4'b10_00});
         #5
 
-        s3 = 0; s2 = 0; s1 = 0; s0 = 1;
-        op[5] = 0; op[4] = 0; op[3] = 0; op[2] = 0; op[1] = 0; op[0] = 0;
+        state[3:0] = 4'b00_01;
+        op[5:0] = 6'b000_000;
         #5
         $display("Test #3");
-        Check1({s3, s2, s1, s0, 4'b00_01});
-        Check2({op[5], op[4], op[3], op[2], op[1], op[0], 6'b000_000});
-        Check1({ns3, ns2, ns1, ns0, 4'b01_10});
+        Check1({state[3:0], 4'b00_01});
+        Check2({op[5:0], 6'b000_000});
+        Check1({ns[3:0], 4'b01_10});
         #5
 
-        s3 = 0; s2 = 0; s1 = 1; s0 = 0;
-        op[5] = 1; op[4] = 0; op[3] = 1; op[2] = 0; op[1] = 1; op[0] = 1;
+        state[3:0] = 4'b00_10;
+        op[5:0] = 6'b101_011;
         #5
         $display("Test #4");
-        Check1({s3, s2, s1, s0, 4'b00_10});
-        Check2({op[5], op[4], op[3], op[2], op[1], op[0], 6'b101_011});
-        Check1({ns3, ns2, ns1, ns0, 4'b01_01});
+        Check1({state[3:0], 4'b00_10});
+        Check2({op[5:0], 6'b101_011});
+        Check1({ns[3:0], 4'b01_01});
         #5
 
-        s3 = 0; s2 = 0; s1 = 1; s0 = 1;
+        state[3:0] = 4'b00_11;
         #5
         $display("Test #5");
-        Check1({s3, s2, s1, s0, 4'b00_11});
-        Check1({ns3, ns2, ns1, ns0, 4'b01_00});
+        Check1({state[3:0], 4'b00_11});
+        Check1({ns[3:0], 4'b01_00});
         #5
 
-        s3 = 0; s2 = 1; s1 = 1; s0 = 0;
+        state[3:0] = 4'b01_10;
         #5
         $display("Test #6");
-        Check1({s3, s2, s1, s0, 4'b01_10});
-        Check1({ns3, ns2, ns1, ns0, 4'b01_11});
+        Check1({state[3:0], 4'b01_10});
+        Check1({ns[3:0], 4'b01_11});
         #5
 
-        s3 = 0; s2 = 0; s1 = 0; s0 = 1;
-        op[5] = 1; op[4] = 0; op[3] = 0; op[2] = 0; op[1] = 1; op[0] = 1;
+        state[3:0] = 4'b00_01;
+        op[5:0] = 6'b100_011;
         #5
         $display("Test #7");
-        Check1({s3, s2, s1, s0, 4'b00_01});
-        Check2({op[5], op[4], op[3], op[2], op[1], op[0], 6'b100_011});
-        Check1({ns3, ns2, ns1, ns0, 4'b00_10});
+        Check1({state[3:0], 4'b00_01});
+        Check2({op[5:0], 6'b100_011});
+        Check1({ns[3:0], 4'b00_10});
         #5
 
-        s3 = 0; s2 = 0; s1 = 0; s0 = 1;
-        op[5] = 1; op[4] = 0; op[3] = 1; op[2] = 0; op[1] = 1; op[0] = 1;
+        state[3:0] = 4'b00_01;
+        op[5:0] = 6'b101_011;
         #5
         $display("Test #8");
-        Check1({s3, s2, s1, s0, 4'b00_01});
-        Check2({op[5], op[4], op[3], op[2], op[1], op[0], 6'b101_011});
-        Check1({ns3, ns2, ns1, ns0, 4'b00_10});
+        Check1({state[3:0], 4'b00_01});
+        Check2({op[5:0], 6'b101_011});
+        Check1({ns[3:0], 4'b00_10});
         #5
 
-        s3 = 0; s2 = 0; s1 = 1; s0 = 0;
-        op[5] = 1; op[4] = 0; op[3] = 0; op[2] = 0; op[1] = 1; op[0] = 1;
+        state[3:0] = 4'b00_10;
+        op[5:0] = 6'b100_011;
         #5
         $display("Test #9");
-        Check1({s3, s2, s1, s0, 4'b00_10});
-        Check2({op[5], op[4], op[3], op[2], op[1], op[0], 6'b100_011});
-        Check1({ns3, ns2, ns1, ns0, 4'b00_11});
+        Check1({state[3:0], 4'b00_10});
+        Check2({op[5:0], 6'b100_011});
+        Check1({ns[3:0], 4'b00_11});
         #5
 
+        $display ("\nErrors: %d", errors);
         $finish;
     end
 endmodule
